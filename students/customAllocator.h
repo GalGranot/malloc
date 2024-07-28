a53fe6f01360296402cb213f27a5ad3f005be7bb
Gal Granot <g_granot@apple.com>
2024-07-28 14:53:34 +0300

    organize

---
+++ students/customAllocator.h
@@ -0,0 +1,35 @@
+#ifndef __CUSTOM_ALLOCATOR__
+#define __CUSTOM_ALLOCATOR__
+
+/*=============================================================================
+* do no edit lines below!
+=============================================================================*/
+#include <stddef.h> //for size_t
+
+void* customMalloc(size_t size);
+void customFree(void* ptr);
+void* customCalloc(size_t nmemb, size_t size);
+void* customRealloc(void* ptr, size_t size);
+/*=============================================================================
+* do no edit lines above!
+=============================================================================*/
+
+/*=============================================================================
+* defines
+=============================================================================*/
+#define SBRK_FAIL (void*)(-1)
+#define ALIGN_TO_8BIT(x) (((((x) - 1) >> 2) << 2) + 4)
+
+/*=============================================================================
+* Block
+=============================================================================*/
+//suggestion for block usage - feel free to change this
+typedef struct Block
+{
+    size_t size;
+    struct Block* next;
+    bool free;
+} Block;
+extern Block* blockList;
+
+#endif // CUSTOM_ALLOCATOR
